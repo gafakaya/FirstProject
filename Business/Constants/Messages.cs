fdc1cccbca2ed4cca3548fbaee025b3e99c2c1f8
Mehmet Kaya <81460760+gafakaya@users.noreply.github.com>
2021-04-22 16:32:04 +0300

    JWT 22/04/21

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,19 +1,21 @@
 ﻿using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
 {
     public static class Messages
     {
-        public static string ProductAdded = "The product is added";
-        public static string ProductNameInvalid = "The product name is invalid";
-        public static string MaintenanceTime = "The system is under maintenance";
-        public static string ProductsListed = "The products is listed";
-        public static string UnitPriceInvalid = "Unit price of the product is invalid";
+        public static string ProductAdded = "The product is added.";
+        public static string ProductNameInvalid = "The product name is invalid.";
+        public static string MaintenanceTime = "The system is under maintenance.";
+        public static string ProductsListed = "The products is listed.";
+        public static string UnitPriceInvalid = "Unit price of the product is invalid.";
         public static string ProductCountOfCategoryError = "There can be a maximum of 10 products in a category.";
         public static string ProductNameAlreadyExistsError = "This product name is already available.";
-        public static string CategoryMaxCountError = "Max category count is must be 15";
+        public static string CategoryMaxCountError = "Max category count is must be 15.";
+        public static string AuthorizationDenied = "Authorization Denied.";
     }
 }
